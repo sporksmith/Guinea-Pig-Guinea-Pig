66ce8cb040529bcd14215b28e0594bd8ae02019f
Jim Newsome <jim@jimnewsome.net>
2013-01-26 23:02:45 -0500

    spawn viruses randomly to the right instead of at spawn point

--- virusSpawnPoint.js
+++ virusSpawnPoint.js
@@ -2,19 +2,26 @@ VirusSpawnPoint = me.InvisibleEntity.extend(
 	{
 		init:function(x,y,settings){
 			this.parent(x,y,settings);
-			this.spawntimer=0;
-			this.spawntick = Math.round(Math.random()*125);
+			this.resetSpawnTimer();
 		},
+
+		resetSpawnTimer:function(){
+			this.spawntimer=Math.round(Math.random()*100);
+		},
+
 		update:function(){
 			//console.log("spawnpoint update");
-			this.spawntimer++;
-			if(this.spawntick==this.spawntimer){
-				var obj = new VirusEntity(this.pos.x, this.pos.y)
+			this.spawntimer--;
+			if(this.spawntimer < 0){
+				var y = Math.round(Math.random() * me.game.viewport.height);
+				var x = me.game.viewport.pos.x + me.game.viewport.width + 100;
+				var obj = new VirusEntity(x, y)
 				// add the object and give the z index of the current object
 				me.game.add(obj,3);
 				// sort the object list (to ensure the object is properly displayed)
 				me.game.sort();
-				this.spawntimer=0;
+				this.resetSpawnTimer();
+				console.log('spawn');
 			}
 		}
 		
